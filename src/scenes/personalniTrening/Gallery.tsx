187f9a76522353717a8853c4af2c00961139b60d
KalabaMile <mile.kalaba7@gmail.com>
2023-02-06 12:30:28 +0100

    gallery-animations

--- src/scenes/personalniTrening/Gallery.tsx
+++ src/scenes/personalniTrening/Gallery.tsx
@@ -2,106 +2,152 @@ import ImageModal from "@/shared/ImageModal";
 import gym from "@/assets/gym.jpg";
 import HomePageGraphic from "@/assets/HomePageGraphic.png";
 import transformacija1 from "@/assets/transformacija1.png";
+import { motion } from "framer-motion";
 
 const Gallery = () => {
     return (
         <div className="container py-2 mx-auto lg:pt-24 lg:px-32">
             <div className="flex flex-wrap -m-1 md:-m-2">
                 <div className="flex flex-wrap w-1/2">
-                    <div className="w-1/2 p-1 md:p-2">
+                    <div className="w-1/2 p-1 md:p-2 relative group">
                         <ImageModal image={HomePageGraphic}
                             parent={
-                                <a role="button">
-                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                        src={gym}>
-                                    </img>
-                                </a>}
+                                <motion.div
+                                whileTap={{ scale: 0.9 }}
+                                >
+                                    <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-blue-800 rounded-lg blur opacity-10 group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
+                                    <a role="button">
+                                        <img alt="gallery" className="relative block object-cover object-center w-full h-full rounded-lg"
+                                            src={gym}>
+                                        </img>
+                                    </a>
+                                </motion.div>}
                         />
                     </div>
-                    <div className="w-1/2 p-1 md:p-2">
+                    <div className="w-1/2 p-1 md:p-2 relative group">
                         <ImageModal image={HomePageGraphic}
                             parent={
-                                <a role="button">
-                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                        src={gym}>
-                                    </img>
-                                </a>}
+                                <motion.div
+                                whileTap={{ scale: 0.9 }}
+                                >
+                                    <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-blue-800 rounded-lg blur opacity-10 group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
+                                    <a role="button">
+                                        <img alt="gallery" className="relative block object-cover object-center w-full h-full rounded-lg"
+                                            src={gym}>
+                                        </img>
+                                    </a>
+                                </motion.div>}
                         />
                     </div>
-                    <div className="w-full p-1 md:p-2">
+                    <div className="w-full p-1 md:p-2 relative group">
                         <ImageModal image={transformacija1}
                             parent={
-                                <a role="button">
-                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                        src={transformacija1}>
-                                    </img>
-                                </a>}
+                                <motion.div
+                                    whileTap={{ scale: 0.9 }}
+                                    >
+                                    <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-blue-800 rounded-lg blur opacity-10 group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
+                                    <a role="button">
+                                        <img alt="gallery" className="relative block object-cover object-center w-full h-full rounded-lg"
+                                            src={transformacija1}>
+                                        </img>
+                                    </a>
+                                </motion.div>}
                         />
                     </div>
                 </div>
                 <div className="flex flex-wrap w-1/2">
-                    <div className="w-full p-1 md:p-2">
+                    <div className="w-full p-1 md:p-2 relative group">
                         <ImageModal image={HomePageGraphic}
                             parent={
-                                <a role="button">
-                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                        src={gym}>
-                                    </img>
-                                </a>}
+                                <motion.div
+                                whileTap={{ scale: 0.9 }}
+                                >
+                                    <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-blue-800 rounded-lg blur opacity-10 group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
+                                    <a role="button">
+                                        <img alt="gallery" className="relative block object-cover object-center w-full h-full rounded-lg"
+                                            src={gym}>
+                                        </img>
+                                    </a>
+                                </motion.div>}
                         />
                     </div>
-                    <div className="w-1/2 p-1 md:p-2">
+                    <div className="w-1/2 p-1 md:p-2 relative group">
                         <ImageModal image={HomePageGraphic}
                             parent={
-                                <a role="button">
-                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                        src={gym}>
-                                    </img>
-                                </a>}
+                                <motion.div
+                                whileTap={{ scale: 0.9 }}
+                                >
+                                    <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-blue-800 rounded-lg blur opacity-10 group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
+                                    <a role="button">
+                                        <img alt="gallery" className="relative block object-cover object-center w-full h-full rounded-lg"
+                                            src={gym}>
+                                        </img>
+                                    </a>
+                                </motion.div>}
                         />
                     </div>
-                    <div className="w-1/2 p-1 md:p-2">
+                    <div className="w-1/2 p-1 md:p-2 relative group">
                         <ImageModal image={HomePageGraphic}
                             parent={
-                                <a role="button">
-                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                        src={gym}>
-                                    </img>
-                                </a>}
+                                <motion.div
+                                whileTap={{ scale: 0.9 }}
+                                >
+                                    <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-blue-800 rounded-lg blur opacity-10 group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
+                                    <a role="button">
+                                        <img alt="gallery" className="relative block object-cover object-center w-full h-full rounded-lg"
+                                            src={gym}>
+                                        </img>
+                                    </a>
+                                </motion.div>}
                         />
                     </div>
                 </div>
             </div>
-            <div className="flex flex-wrap -m-1 md:-m-2">
+            <div className="py-1 flex flex-wrap -m-1 md:-m-2">
                 <div className="flex flex-wrap">
-                    <div className="w-1/3 p-1 md:p-2">
+                    <div className="w-1/3 p-1 md:p-2 relative group">
                         <ImageModal image={HomePageGraphic}
                             parent={
-                                <a role="button">
-                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                        src={gym}>
-                                    </img>
-                                </a>}
+                                <motion.div
+                                whileTap={{ scale: 0.9 }}
+                                >
+                                    <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-blue-800 rounded-lg blur opacity-10 group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
+                                    <a role="button">
+                                        <img alt="gallery" className="relative block object-cover object-center w-full h-full rounded-lg"
+                                            src={gym}>
+                                        </img>
+                                    </a>
+                                </motion.div>}
                         />
                     </div>
-                    <div className="w-1/3 p-1 md:p-2">
+                    <div className="w-1/3 p-1 md:p-2 relative group">
                         <ImageModal image={HomePageGraphic}
                             parent={
-                                <a role="button">
-                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                        src={gym}>
-                                    </img>
-                                </a>}
+                                <motion.div
+                                whileTap={{ scale: 0.9 }}
+                                >
+                                    <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-blue-800 rounded-lg blur opacity-10 group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
+                                    <a role="button">
+                                        <img alt="gallery" className="relative block object-cover object-center w-full h-full rounded-lg"
+                                            src={gym}>
+                                        </img>
+                                    </a>
+                                </motion.div>}
                         />
                     </div>
-                    <div className="w-1/3 p-1 md:p-2">
+                    <div className="w-1/3 p-1 md:p-2 relative group">
                         <ImageModal image={HomePageGraphic}
                             parent={
-                                <a role="button">
-                                    <img alt="gallery" className="block object-cover object-center w-full h-full rounded-lg"
-                                        src={gym}>
-                                    </img>
-                                </a>}
+                                <motion.div
+                                whileTap={{ scale: 0.9 }}
+                                >
+                                    <div className="absolute -inset-1 bg-gradient-to-r from-red-600 to-blue-800 rounded-lg blur opacity-10 group-hover:opacity-100 transition duration-500 group-hover:duration-100"></div>
+                                    <a role="button">
+                                        <img alt="gallery" className="relative block object-cover object-center w-full h-full rounded-lg"
+                                            src={gym}>
+                                        </img>
+                                    </a>
+                                </motion.div>}
                         />
                     </div>
                 </div>
